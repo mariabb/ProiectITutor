ed3a7cdd86310c560d9a765f45965e20709c967d
MARIA BUFNEA <mariabufnea@gmail.com>
2020-02-25 12:00:18 +0200

    tema7 PHP

--- tema7/PHP/tema7.php
+++ tema7/PHP/tema7.php
@@ -13,8 +13,9 @@ echo "<br />";
 $cities = array('Cluj Napoca', 'Timisoara', 'Constanta', "Pitesti");
 echo $cities[2];
 echo "<br />";
-$zi = array("luni", "marti", "miercuri", "joi", "vineri", "sambata", "duminica"); 
-echo "Zilele saptamanii sunt: " . $zi[0] . ", " . $zi[1] ."," . $zi[2] . ", " . $zi[3] .", " . $zi[4] . "," .$zi[5] ." si " . $zi[6]."." ;
+//indexed array
+$zi = array("luni", "marti", "miercuri", "joi", "vineri", "sambata", "duminica");
+echo "Zilele saptamanii sunt: " . $zi[0] . ", " . $zi[1] . "," . $zi[2] . ", " . $zi[3] . ", " . $zi[4] . "," . $zi[5] . " si " . $zi[6] . ".";
 echo "<br />";
 $numerals = [1, 2, 3, 100, 84, 6, 5];
 sort($numerals);
@@ -28,6 +29,23 @@ $sports = ['fotbal', 'gimnastica', 'tenis'];
 sort($sports);
 print_r($sports);
 echo "<br />";
+$age['Ana'] = "20";
+$age['Letitia'] = "30";
+$age['Ileana'] = "25";
+echo "Letitia is " . $age['Letitia'] . " years old.";
+echo "<br />";
+$age = array("Ana" => "20", "Letitia" => "30", "Ileana" => "25");
+asort($age);
+print_r($age);
+echo "<br />";
+echo "<br />";
+$myArray = array(
+    array("ianuarie", "februarie", "martie"),
+    array("aprilie", "mai", "iunie")
+);
+// Displays "iunie"
+echo $myArray[1][2];
+echo "<br />";
 function Name($name, $birthYear)
 {
     echo "$name Popescu. Born in $birthYear <br>";
@@ -36,8 +54,7 @@ Name('Ana', 1970);
 Name('Marcel', 1971);
 Name('Anastasia', 1980);
 Name('Corina', 1975);
-
-$age = array("Ana"=>"50", "Marcel"=>"49", "Anastasia"=>"40");
-echo "Marcel is " . 
-$age['Marcel']
- . " years old.";
+$age = array("Ana" => "50", "Marcel" => "49", "Anastasia" => "40");
+echo "Marcel is " .
+    $age['Marcel']
+    . " years old.";
