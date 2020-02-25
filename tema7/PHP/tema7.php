dc6285851bb6c822f1a042ff5d25170a77196470
MARIA BUFNEA <mariabufnea@gmail.com>
2020-02-25 10:03:58 +0200

    tema7

--- tema7/PHP/tema7.php
+++ tema7/PHP/tema7.php
@@ -1,22 +1,35 @@
 <?php
-$numerals = [1,2,3,100,84,6,5];
+$cities = array('Cluj Napoca', 'Timisoara', 'Constanta', "Pitesti");
+$length = count($cities);
+
+for ($x = 0; $x < $length; $x++) {
+    echo $cities[$x];
+    echo '<br>';
+}
+echo "<br />";
+$cities = array('Cluj Napoca', 'Timisoara', 'Constanta', "Pitesti");
+echo count($cities);
+echo "<br />";
+$cities = array('Cluj Napoca', 'Timisoara', 'Constanta', "Pitesti");
+echo $cities[2];
+echo "<br />";
+$numerals = [1, 2, 3, 100, 84, 6, 5];
 sort($numerals);
 print_r($numerals);
 echo "<br />";
-$numerals = [1,2,3,100,84,6,5];
+$numerals = [1, 2, 3, 100, 84, 6, 5];
 rsort($numerals);
 print_r($numerals);
 echo "<br />";
- $sports = ['fotbal', 'gimnastica', 'tenis'];
- sort($sports);
- print_r($sports);
- echo "<br />";
- function Name($name, $birthYear) {
+$sports = ['fotbal', 'gimnastica', 'tenis'];
+sort($sports);
+print_r($sports);
+echo "<br />";
+function Name($name, $birthYear)
+{
     echo "$name Popescu. Born in $birthYear <br>";
 }
 Name('Ana', 1970);
 Name('Marcel', 1971);
 Name('Anastasia', 1980);
 Name('Corina', 1975);
-
-?>
