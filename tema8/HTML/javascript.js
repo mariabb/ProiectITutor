69ab3eb26a404f1f8f6896bc427d57f3f4010cf8
MARIA BUFNEA <mariabufnea@gmail.com>
2020-03-01 19:16:05 +0200

    tema8

--- tema8/HTML/javascript.js
+++ tema8/HTML/javascript.js
@@ -7,31 +7,26 @@ var ctx = c.getContext("2d");
 ctx.beginPath();
 ctx.arc(95,50,40,0,2*Math.PI);
 ctx.stroke();
-
 var c = document.getElementById('myCanvas');
 var ctx = c.getContext('2d');
 ctx.beginPath();
 ctx.arc(300,75,50,0*Math.PI,1.5*Math.PI);
 ctx.stroke();
-
 var c = document.getElementById('myCanvas');
 var ctx = c.getContext('2d');
 ctx.beginPath();
 ctx.arc(100,150,50,0*Math.PI,1.5*Math.PI);
 ctx.stroke();
-
 var c = document.getElementById('myCanvas');
 var ctx = c.getContext('2d');
 ctx.beginPath();
 ctx.arc(100,75,50,1*Math.PI,1.5*Math.PI);
 ctx.stroke();
-
 var c = document.getElementById('myCanvas');
 var ctx = c.getContext('2d');
 ctx.beginPath();
 ctx.arc(100,75,50,0*Math.PI,2*Math.PI);
 ctx.stroke();
-
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 ctx.shadowBlur = 10;
@@ -39,8 +34,6 @@ ctx.shadowOffsetY = 20;
 ctx.shadowColor = "black";
 ctx.fillStyle = "blue";
 ctx.fillRect(20, 20, 100, 80);
-
-
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 ctx.shadowBlur = 10;
@@ -48,7 +41,6 @@ ctx.shadowOffsetY = 20;
 ctx.shadowColor = "black";
 ctx.fillStyle = "green";
 ctx.fillRect(100, 100, 100, 80);
-
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 ctx.shadowBlur = 10;
